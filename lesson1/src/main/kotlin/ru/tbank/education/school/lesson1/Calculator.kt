ebeb88edb7df5d7da21ee0f29b3a50ece5962296
Михаил Сергеев <mikhail.sergeev2010@gmail.com>
2025-09-12 17:52:39 +0300

    Домашнее_задание_1

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
@@ -3,10 +3,26 @@ package ru.tbank.education.school.lesson1
 /**
  * Метод для вычисления простых арифметических операций.
  */
-fun calculate(a: Double, b: Double, operation: OperationType): Double? {
-    TODO()
+fun calculate(
+    a: Double, b: Double, operation: OperationType = OperationType.ADD): Double? {
+    val result = when (operation) {
+        OperationType.ADD -> a + b
+        OperationType.SUBTRACT -> a - b
+        OperationType.MULTIPLY -> a * b
+        OperationType.DIVIDE -> if (b != 0.0) a / b else null
+    }
+
+    result?.let { println("$it") }
+        ?: println("Ошибка: деление на ноль невозможно")
+    return result
 }
 
+
+fun main() {
+    calculate(10.0, 5.0)
+    calculate(10.0, 5.0, OperationType.MULTIPLY)
+    calculate(10.0, 0.0, OperationType.DIVIDE)
+}
 /**
  * Функция вычисления выражения, представленного строкой
  * @return результат вычисления строки или null, если вычисление невозможно
--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
@@ -4,5 +4,17 @@ package ru.tbank.education.school.lesson1
  * Сумма четных чисел.
  */
 fun sumEvenNumbers(numbers: Array<Int>): Int {
-    TODO()
+    var sum = 0
+    for (i in numbers) {
+        if (i % 2 == 0) {
+            sum += i
+        }
+    }
+    return sum
+}
+
+fun main(){
+    val numbers = arrayOf(1, 2, 3, 4, 5, 6)
+    val result = sumEvenNumbers(numbers)
+    println(result)
 }
